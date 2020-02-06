3d138f0793256edc0f5625074fac72846ba518a2
Derek Hargest <derek.h@rollwithduckpin.com>
2020-02-06 11:44:01 -0500

    Adding Gulp and Legacy to build file

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ composer_path="./composer.json"
 package_path="./package.json"
 build_file_path="./gulpfile.js"
 bower_file_path="./bower.json"
-build_type=webpack
+
+webpack_path="./build/base.config.js"
+webpack_path_legacy="./build/config/base.config.js"
 
 # Begin from the ~/clone directory
 # this directory is the default your git project is checked out into by Codeship.
@@ -22,6 +24,22 @@ then
 	composer install
 fi
 
+if [ -f "$build_file_path" ]
+then
+	echo "Gulpfile found. Starting build process"
+	build_type=gulp
+fi
+
+if [-f "$webpack_path"]
+then
+	build_type = "webpack"
+fi
+
+if [-f "$webpack_path_legacy"]
+then
+	build_type = "webpack"
+fi
+
 # check to see our build type and if so build using either gulp or grunt
 if [ "$build_type" != "none" ]
 then
@@ -32,6 +50,52 @@ then
 		yarn install
 		echo "Deploy Webpack"
 		yarn deploy
+	fi
+fi
+
+# check to see our build type and if so build using either gulp or grunt
+if [ "$build_type" != "none" ]
+then
+	if [ "$build_type" == "gulp_yarn" ]
+	then
+		echo "Yarn Install"
+		yarn global add gulp-cli
+		yarn install
+	
+		# Only build if the build:production task exists in the build path
+		if grep -q build:production "$build_file_path";
+		then
+			echo "Building project using gulp"
+			gulp build:production
+		fi
+	else
+	    echo "Initiating NPM Install"
+	    npm install
+
+	    # Only install and fire bower if we have a bower.json
+	    if [ -f "$bower_file_path" ]
+	    then
+		echo "Initiating Bower Install"
+
+		npm install -g bower
+		bower install
+	    fi
+
+	    if [ $build_type = "gulp" ]
+	    then
+		    if grep -q build:production "$build_file_path";
+			then
+			echo "Building project using gulp"
+			gulp build:production
+		    fi
+	    else
+	    	    # Make sure we have a build command within our grunt file
+		    if grep -q build "$build_file_path";
+			then
+			echo "Building project using grunt"
+			grunt build
+		    fi
+	    fi
 	fi
 fi
 
