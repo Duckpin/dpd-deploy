c4b03055626aca25029a8cbebc01401fd57ebbd2
Derek Hargest <derek.h@rollwithduckpin.com>
2020-03-18 07:15:45 -0400

    deployprod

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ then
 			echo "Webpack File Found. Starting Yarn Install"
 			yarn install
 			echo "Deploy Webpack"
-			yarn deploy
+			yarn deployprod
 		else
 	    echo "Initiating Yarn Install"
 	    yarn install
