3c623b7c5117898dc794a19c85149f67f9264675
Grant Black <grant.b@rollwithduckpin.com>
2020-12-18 17:02:14 -0500

    try install composer here

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,9 @@ cd ${working_directory:-./}
 
 build_type=webpack
 
+echo "Starting Composer Install"
+composer install
+
 # check to see our build type and if so build using either gulp or grunt
 if [ "$build_type" != "none" ]
 then
